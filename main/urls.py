7f9c4a60be2b6aaf530c36b39cec73a649eacc94
Shantharam P <shanthu2005best@gmail.com>
2025-07-21 23:10:00 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/privacy', views.privacy, name='privacy'),
-    path('/terms', views.terms, name='terms'),
-    path('/shipping', views.shipping, name='shipping'),
-    path('/refund_policy', views.refund, name='refund_policy'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
+    path('shipping/', views.shipping, name='shipping'),
+    path('refund_policy/', views.refund, name='refund_policy'),
 ]
